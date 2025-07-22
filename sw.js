9fcb91803c03c4e98fac882e8a05e8dfc62135d3
redrednatunatu <redietf784@gmail.com>
2025-07-22 15:48:01 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -9,14 +9,18 @@ const assets = [
   './icons/logo.svg'
 ];
 
-self.addEventListener('install', e => {
-  e.waitUntil(
-    caches.open(cacheName).then(cache => cache.addAll(assets))
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(cacheName).then(cache => {
+      return cache.addAll(assets);
+    })
   );
 });
 
-self.addEventListener('fetch', e => {
-  e.respondWith(
-    caches.match(e.request).then(res => res || fetch(e.request))
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
   );
 });
